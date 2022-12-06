7a2b2c11a5ae828383ac51509dd8a13b857c64b9
luis-ponce <luisfponcinho@gmail.com>
2022-12-06 17:43:32 -0500

    second edition

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+install_requires = [
+    'pandas', 
+    'streamlit==0.082.0',
+    'molmass',
+    'ms-mint'
+]
+
+config = {
+    'description': 'qc_pipeline',
+    'author': 'luis-ponce and soren-wacker',
+    'url': 'https://github.com/luis-ponce',
+    'download_url': 'https://github.com/luis-ponce/ms_mint_conc',
+    'author_email': 'luisfponcinho@gmail.com',
+    'version': '0.0.1',
+    'install_requires': install_requires,
+    'packages': find_packages(),
+    'scripts': [],
+    'name': 'QC_metabolomics'
+}
+
+setup(**config)
