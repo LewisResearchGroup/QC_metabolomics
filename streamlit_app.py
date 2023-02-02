07cc4ee3d9fbc44fb565b9d9f6a849cbfa5322b6
luis-ponce <luisfponcinho@gmail.com>
2023-02-02 13:28:59 -0500

    5th edition

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,10 +10,54 @@ import numpy as np
 import glob
 import re
 import streamlit as st
-from stlibs import SessionState
+# from stlibs import SessionState
 
 from streamlit.server.server import Server
 import streamlit.report_thread as ReportThread
+class SessionState(object):
+    def __init__(self, **kwargs):
+        for key, val in kwargs.items():
+            setattr(self, key, val)
+            
+def get(**kwargs):
+
+    ctx = ReportThread.get_report_ctx()
+
+    this_session = None
+
+    current_server = Server.get_current()
+    if hasattr(current_server, '_session_infos'):
+        # Streamlit < 0.56
+        session_infos = Server.get_current()._session_infos.values()
+    else:
+        session_infos = Server.get_current()._session_info_by_id.values()
+
+    for session_info in session_infos:
+        s = session_info.session
+        if (
+            # Streamlit < 0.54.0
+            (hasattr(s, '_main_dg') and s._main_dg == ctx.main_dg)
+            or
+            # Streamlit >= 0.54.0
+            (not hasattr(s, '_main_dg') and s.enqueue == ctx.enqueue)
+            or
+            # Streamlit >= 0.65.2
+            (not hasattr(s, '_main_dg') and s._uploaded_file_mgr == ctx.uploaded_file_mgr)
+        ):
+            this_session = s
+
+    if this_session is None:
+        raise RuntimeError(
+            "Oh noes. Couldn't get your Streamlit Session object. "
+            'Are you doing something fancy with threads?')
+
+    # Got the session object! Now let's attach some state into it.
+
+    if not hasattr(this_session, '_custom_session_state'):
+        this_session._custom_session_state = SessionState(**kwargs)
+
+    return this_session._custom_session_state
+
 # In[3]:
 
 
@@ -62,7 +106,7 @@ st.write('### Upload the results file generated on Mint')
 results_file = ''
 results_file = st.file_uploader('results file')
 if results_file != '':
-    s_st = SessionState.get(results = pd.read_csv(results_file))
+    s_st = get(results = pd.read_csv(results_file))
     st.write(s_st.results.head())
 
 # try:
